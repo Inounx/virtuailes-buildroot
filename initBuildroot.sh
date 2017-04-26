4e4d8aad744cc7c6ac08a4ba7c9b2f6685c0e12a
Inounx <inounx@gmail.com>
2017-04-26 17:45:19 +0200

    Ajout des fichiers de structure pour les external build root

---
+++ initBuildroot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+#Absolute path this script is in, thus /home/user/bin
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+cd $SCRIPT_PATH/beaglebone_external/package
+echo "Cloning external buildroot package vpbus..."
+git clone https://github.com/Inounx/vpbus.git vpbus
+
+echo "Cloning external buildroot package vpbus_gpiolib..."
+git clone -b gpiolib https://github.com/Inounx/vpbus.git vpbus_gpiolib
+
+echo "Cloning external buildroot package vpbus_test..."
+git clone https://github.com/Inounx/vpbus-test.git vpbus_test
+
+echo "Cloning buildroot repository..."
+git clone http://git.buildroot.net/git/buildroot.git $SCRIPT_PATH/buildroot
+cd $SCRIPT_PATH/buildroot
+git checkout -b virtuailes 2016.05
+
+mkdir $SCRIPT_PATH/output
+
+echo "Configuring buildroot..."
+make -C $SCRIPT_PATH/buildroot \
+     -O $SCRIPT_PATH/output \
+     BR2_EXTERNAL=$SCRIPT_PATH/beaglebone_external \
+     virtuailes_beaglebone_defconfig
+
+echo "Buildroot initialization done!"
